43043ef45aeeff7b2d53de7ff7b301c2632b6a34
Jeremy Kanal-Scott <jkanalscott@gmail.com>
2023-09-24 03:09:47 +1300

    Basic interface for GameWindow.

--- src/main/java/nz/ac/wgtn/swen225/lc/renderer/GameWindow.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/renderer/GameWindow.java
@@ -1,6 +1,27 @@
 package nz.ac.wgtn.swen225.lc.renderer;
 
-import java.awt.*;
+import nz.ac.wgtn.swen225.lc.domain.Game;
 
-public class GameWindow extends Component {
+import javax.swing.*;
+
+
+/**
+ * Displays a portion of the maze centered around the player.
+ */
+public class GameWindow extends JViewport {
+    /**
+     * Constructor.
+     *
+     * @param game       The game world to be rendered.
+     * @param windowSize Size-length of this component. Unused.
+     */
+    public GameWindow(final Game game, final int windowSize) {
+        //TODO: stub
+    }
+
+    @Override
+    public final void setEnabled(final boolean enabled) {
+        //TODO: stub. Should only subscribe
+        super.setEnabled(enabled);
+    }
 }
