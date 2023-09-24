8f6415a3e4b2356665f94236dbb3220a89182b5a
Jeremy Kanal-Scott <jkanalscott@gmail.com>
2023-09-24 17:11:05 +1300

    Changed GameWindow to extend JPanel, modified constructors, added javadoc for setEnabled.

--- src/main/java/nz/ac/wgtn/swen225/lc/renderer/GameWindow.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/renderer/GameWindow.java
@@ -3,25 +3,56 @@ package nz.ac.wgtn.swen225.lc.renderer;
 import nz.ac.wgtn.swen225.lc.domain.Game;
 
 import javax.swing.*;
+import java.awt.*;
 
 
 /**
- * Displays a portion of the maze centered around the player.
+ * Exports the view of the maze to other modules.
  */
-public class GameWindow extends JViewport {
+public class GameWindow extends JPanel {
     /**
      * Constructor.
      *
-     * @param game       The game world to be rendered.
-     * @param windowSize Size-length of this component. Unused.
+     * <p>Only shows the maze when enabled.
+     *
+     * @param game The game world to be rendered.
      */
-    public GameWindow(final Game game, final int windowSize) {
-        //TODO: stub
+    public GameWindow(final Game game) {
+        setEnabled(false);
+        //TODO:stub
     }
 
+    /**
+     * Constructs with the given preferred size.
+     *
+     * @param game       The game world to be rendered.
+     * @param windowSize Preferred length of this panel.
+     */
+    public GameWindow(final Game game, final int windowSize) {
+        this(game);
+        setSize(windowSize, windowSize);
+    }
+
+    /**
+     * Sets whether this component is enabled. A component that is enabled may respond to user
+     * input, while a component that is not enabled cannot respond to user input.  Some components
+     * may alter their visual representation when they are disabled in order to provide feedback to
+     * the user that they cannot take input.
+     *
+     * <p>Enabling this component allows it to update to game events. This component is disabled by
+     * default.
+     *
+     * <p>Note: Disabling a component does not disable its children.
+     *
+     * <p>Note: Disabling a lightweight component does not prevent it from
+     * receiving MouseEvents.
+     *
+     * @param enabled true if this component should be enabled, false otherwise
+     * @see Component#isEnabled
+     * @see Component#isLightweight
+     */
     @Override
-    public final void setEnabled(final boolean enabled) {
-        //TODO: stub. Should only subscribe
+    public void setEnabled(final boolean enabled) {
         super.setEnabled(enabled);
     }
 }
