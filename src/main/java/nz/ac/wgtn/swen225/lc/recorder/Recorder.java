8c41719f5ed4669619705ff4e99fbf4c00752a1b
Sajja Syed <syedsajj@ecs.vuw.ac.nz>
2023-10-11 05:17:35 +1300

    Added java doc and student name and ID

--- src/main/java/nz/ac/wgtn/swen225/lc/recorder/Recorder.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/recorder/Recorder.java
@@ -5,8 +5,32 @@ import nz.ac.wgtn.swen225.lc.domain.level.characters.Enemy;
 
 import java.util.Map;
 
+/**
+ * The Recorder interface defines methods for recording and managing game actions or events.
+ * Implementations of this interface can be used to record player movements and enemy movements
+ * within a game.
+ *
+ * @author Sajja Syed 300551462
+ */
 public interface Recorder {
+
+    /**
+     * Called when the recording starts.
+     * Implementations should perform any necessary setup or initialization here.
+     */
     void onStart();
+
+    /**
+     * Updates the recorder with player and enemy movements during gameplay.
+     *
+     * @param playerMovement      The movement vector representing the player's movement.
+     * @param enemyMovementMap   A mapping of enemies to their respective movement vectors.
+     */
     void update(Vector2D playerMovement, Map<Enemy, Vector2D> enemyMovementMap);
+
+    /**
+     * Called when the recording is being stopped or destroyed.
+     * Implementations should perform any cleanup or resource release here.
+     */
     void onDestroy();
 }
