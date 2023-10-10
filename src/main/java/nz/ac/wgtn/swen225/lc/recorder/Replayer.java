e4f74bd64d03f92271def5c5c8f69f5cc16dd7ac
Sajja Syed <syedsajj@ecs.vuw.ac.nz>
2023-10-11 05:17:08 +1300

    Added java doc and student name and ID

--- src/main/java/nz/ac/wgtn/swen225/lc/recorder/Replayer.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/recorder/Replayer.java
@@ -1,6 +1,23 @@
 package nz.ac.wgtn.swen225.lc.recorder;
 
+/**
+ * The Replayer interface defines methods for starting and destroying a replayer.
+ * Implementations of this interface can be used to record and replay different actions
+ * or events in the program.
+ *
+ * @author Sajja Syed 300551462
+ */
 public interface Replayer {
+
+    /**
+     * Called when the replayer is started.
+     * Implementations should perform any necessary setup or initialization here.
+     */
     void onStart();
+
+    /**
+     * Called when the replayer is being destroyed or stopped.
+     * Implementations should perform any cleanup or resource release here.
+     */
     void onDestroy();
 }
