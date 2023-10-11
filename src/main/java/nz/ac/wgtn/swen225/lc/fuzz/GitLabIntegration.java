7e68499adef82e7b13b411235e57b009967cd1fd
Oliver Berry <berryoliv1@ecs.vuw.ac.nz>
2023-10-11 21:06:34 +1300

    Adding comments and documentation

--- src/main/java/nz/ac/wgtn/swen225/lc/fuzz/GitLabIntegration.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/fuzz/GitLabIntegration.java
@@ -7,7 +7,22 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+/**
+ * This class enables the fuzz tests to automatically create issues in gitlab
+ *
+ * @author Oliver Berry
+ * Student ID: 300474410
+ */
+
 public class GitLabIntegration {
+
+    /**
+     * creates an issue in the gitlab and assigns it to a user
+     *
+     * @param title the title for the issue
+     * @param description the description of the issue
+     * @param assignID the user id for the issue to be assigned to
+     */
 
     public static void reportIssueWithAssign(String title, String description, String assignID) {
         try {
@@ -38,6 +53,16 @@ public class GitLabIntegration {
             throw new IllegalStateException("Failed to communicate to gitlab", e);
         }
     }
+
+    /**
+     * creates an issue in the gitlab, but doesn't assign it to a user.
+     * Used in cases where the fuzzer fails to assign an error to a user,
+     * because it is still valuable to know that an error was caused and
+     * what the error was.
+     *
+     * @param title the title for the issue
+     * @param description the description of the issue
+     */
 
     public static void reportIssueNoAssign(String title, String description) {
         try {
